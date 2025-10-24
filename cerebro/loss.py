0a600fc73ece528671e96783dcfff2997619b922
tutur90 <arthur.garon@gmail.com>
2025-10-24 14:18:26 +0200

    loss

--- cerebro/loss.py
+++ cerebro/loss.py
@@ -74,41 +74,28 @@ class InvLoss(torch.nn.Module):
     def __init__(self):
         super().__init__()
 
-    def forward(self, output: torch.Tensor, target: torch.Tensor, rev_in: RevIn, leverage=1, grid_scale: float=1.0, num_items_in_batch: int= None) -> torch.Tensor:
+    def forward(self, output: torch.Tensor, target: torch.Tensor, rev_in: RevIn, leverage=1, grid_scale: float=1.0,  num_items_in_batch: int= None) -> torch.Tensor:
         
 
-        # if output.shape != target.shape:
-        #     raise ValueError(f"Output shape {output.shape} does not match target shape {target.shape}")
-
         scale = rev_in.scale * grid_scale
+        
+        grid = torch.linspace(-1, 1, steps=output.shape[-1], device=output.device).unsqueeze(0) * scale.unsqueeze(1)   # (B, D)
+        
+        inv = torch.softmax(output, dim=-1) * torch.sign(grid)
+        
+        # inv = output/output.abs().sum(dim=-1, keepdim=True) * leverage
 
-        # print("scale", scale.shape)
-
-        grid = torch.linspace(-1, 1, steps=output.shape[-1], device=output.device).unsqueeze(0) * scale.unsqueeze(1) + rev_in.last.view(-1, 1)  # (B, D)
+        inv = inv * taken_order * leverage
+        
+        grid = grid + rev_in.last.view(-1, 1)  # (B, D)
 
         ret = target[:, 2].unsqueeze(-1)/( grid)  # (B, T)
 
 
         taken_order = (grid > target[:, 0].unsqueeze(-1).repeat(1, grid.shape[1])) * (grid < target[:, 1].unsqueeze(-1).repeat(1, grid.shape[1]))  # (B, D)
 
-
-        # print("ret", ret)
-
-        # print("taken_order", taken_order.sum(dim=1))
-
-        inv = output/output.abs().sum(dim=-1, keepdim=True) * leverage * taken_order
-        
-        # print("inv", inv)
-
-
-        pnl = (inv * (ret - 1)).mean(dim=-1) + 1  # (B, T)
+        pnl = (inv * (ret - 1)).sum(dim=-1) + 1  # (B, T)
 
         log_pnl = torch.log(pnl.clamp(min=1e-8))
-
-        # print("log_pnl", log_pnl)
-        
-        # inv = output.mean(dim=-1) * (target[:, :, 2]-last.view(-1, 1)) / (last.view(-1, 1)) 
-        # inv = -torch.log(1 + inv.clamp(min=-0.999))  # prevent log(0)
-
 
         return -log_pnl.mean() * 24 * 364  # minimize negative log-pnl
