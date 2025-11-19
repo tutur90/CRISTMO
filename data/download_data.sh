b2bc91baaea930d87f24790d6cf87f72cbc30dbe
Arthur Garon <arthurgaron@MacBook-Air-de-Arthur.local>
2025-11-19 14:50:18 +0100

    s

--- data/download_data.sh
+++ data/download_data.sh
@@ -10,7 +10,7 @@ intervals=("1m")
 years=("2017" "2018" "2019" "2020" "2021" "2022" "2023" "2024" "2025")
 months=("01" "02" "03" "04" "05" "06" "07" "08" "09" "10" "11" "12")
 type="futures" # "spot" or "futures"
-max_parallel=32  # Adjust based on your system and network
+max_parallel=128  # Adjust based on your system and network
 
 # Set base URL
 if [[ "${type}" == "spot" ]]; then
